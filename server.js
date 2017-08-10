a1a932ad3fb452c131be73147dca223f29b8cafc
Keerthana2498 <30742470+Keerthana2498@users.noreply.github.com>
2017-08-10 21:48:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.get('/article-one', function(req, res){
     res.send('article one will be served here');
 });
 
-app.get('/article two', function(req,res){
+app.get('/article-two', function(req,res){
     res.send('article two will be served here');
 });
 
-app.get('/article three', function(req,res){
+app.get('/article-three', function(req,res){
     res.send('article three will be served here');
 });
 
