c221445e4334f2ca0d56d80bd1b80c366594e2b4
Keerthana2498 <30742470+Keerthana2498@users.noreply.github.com>
2017-08-11 23:41:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,48 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleOne = {
+    title: 'Article-one has started',
+    heading:'Article-one',
+    content:`<p>
+                this is a web app. this is a web app. this is a web app. 
+                </p>
+            <p>
+                        how come how come how come how come
+                    </p>`
+};
+function createTemplate(data) {
+    var title=data.title;
+    var heading=data.heading;
+    var content=data.content;
 
+var htmlTemplate =
+`<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+    
+    <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+        <body>
+            <div class="container">
+            <div>
+                <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h1>
+                   ${heading}
+                </h1>
+                <div>
+            ${content}
+                </div>
+            </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -14,7 +55,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.sendFile(createTemplate(articleOne));
     
 });
 
