80f7ab9a38301cd1c425baeedcea78164b924c31
Keerthana2498 <30742470+Keerthana2498@users.noreply.github.com>
2017-08-19 04:05:59 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,1 @@
 console.log('Loaded!');
-var element=document.getElementById("main-text");
-element.innerHTML="new hfjd";
-var marginLeft=0;
-
-    
-var hk=document.getElementById("img");
-hk.onclick=function(){
-    hk.style.marginLeft=marginLeft+"100px";
-    
-};
