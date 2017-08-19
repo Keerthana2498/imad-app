f4ae162bfc0da193b447fbf3791cd08a025e41b6
Keerthana2498 <30742470+Keerthana2498@users.noreply.github.com>
2017-08-19 01:22:09 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,8 +83,8 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 
-app.get('/ui/img', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'img'));
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
