d1e33632a24c9b11f23c1bc3b2908c2564749dcb
Keerthana2498 <30742470+Keerthana2498@users.noreply.github.com>
2017-08-19 04:51:08 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,16 @@ var button=document.getElementById('counter');
 var counter=0;
 
 button.onclick = function(){
-    counter = counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
-};
+    var request=new XMLhttpRequest(){
+        request.onreadystatechange=function(){
+            if(request==XMLhttpRequest.DONE){
+                
+                if(request.status == 200){
+                var counter=request.respnsetext;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+       }
+            }
+        }
+    }
+}
