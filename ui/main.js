9665d174330f03b0d69fddf3ba93291375989c6c
Keerthana2498 <30742470+Keerthana2498@users.noreply.github.com>
2017-08-22 11:01:18 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,8 +18,7 @@ button.onclick = function(){
         request.send(null);
 };
  
- var nameInput=document.getElementById("name");
- var name= nameInput.value;
+ 
  var submit=document.getElementById("submit_btn");
  submit.onclick=function(){ 
     var request=new XMLhttpRequest()
@@ -41,6 +40,8 @@ button.onclick = function(){
             }
         
         };
+       var nameInput=document.getElementById("name");
+       var name= nameInput.value; 
         request.open('GET','http://askcutir.imad.hasura-app.io/submit-name?name'+ name, true );
         request.send(null);
 };
