59a35b9aa98fdf8a44b181c4bcd36dad41fb40f1
Keerthana2498 <30742470+Keerthana2498@users.noreply.github.com>
2017-08-22 10:56:47 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,33 +3,44 @@ var button=document.getElementById('counter');
 
 
 button.onclick = function(){
+     var request = new XMLHttpRequest();
+     request.onreadystatechange = function(){
+         if(request.readyState==XMLHttpRequest.DONE){
+             if(request.readyState==200){
+                 var counter=request.responseText();
+                 var span=document.getElementById('count');
+                 span.innerHTML = counter.toString();
+             }
+         }
+     };
+     
+    request.open('GET','http://askcutir.imad.hasura-app.io/counter', true );
+        request.send(null);
+};
+ 
+ var nameInput=document.getElementById("name");
+ var name= nameInput.value;
+ var submit=document.getElementById("submit_btn");
+ submit.onclick=function(){ 
     var request=new XMLhttpRequest()
         request.onreadystatechange=function(){
             if(request==XMLhttpRequest.DONE){
                 
                 if(request.status == 200){
-                var counter=request.respnsetext;
-                var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
+                    var names=request.responseText;
+                    names=JSON.parse(names);
+                   list='';
+                for(var i=0;i<names.length;i++){
+              list += '<li>' + 'names[i]' + '</li>'
+         }
+      var ul=document.getElementById("namelist");
+     ul.innerHTML=list;
+     
+               
        }
             }
         
         };
-        request.open('GET','http://a
-        askcutir.imad.hasura-app.io/counter', true );
+        request.open('GET','http://askcutir.imad.hasura-app.io/submit-name?name'+ name, true );
         request.send(null);
-};
-
-var nameInput=document.getElementById("name");
-var name= nameInput.value;
-var submit=document.getElementById("submit_btn");
-submit.onclick=function(){
-    var names=['name1', 'name2', 'name3'];
-    list='';
-    for(var i=0;i<names.length;i++){
-        list += '<li>' + 'names[i]' + '</li>'
-    }
-var ul=document.getElementById("namelist");
-ul.innerHTML=list;
-    
 };
