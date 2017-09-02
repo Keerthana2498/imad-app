a865cb5c2215129c790abdfec2b5e2a2e2c91690
Keerthana2498 <30742470+Keerthana2498@users.noreply.github.com>
2017-09-02 11:18:26 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,12 +104,26 @@ app.get('/submit-name',function(req,res){
 });
 
 
-app.get('/:articleName', function(req, res){
+app.get('/articles/:articleName', function(req, res){
    //articleName == article-one
    //articles[articleName] = {}  content object for article-one
+   pool.query("SELECT *FROM article WHERE title='"+req.params.articleName+'"',function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+               }
+               else{
+                   
+               if('result.rows.length'==0){
+                   res.status(404).send('article not found');
+               }
+               else{
+                   var articleData=result.rows[0];
+                   res.send(createTemplate(articleData));
+               }
    var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName]));
     
+});
 });
 
 app.get('/ui/main.js', function (req, res) {
